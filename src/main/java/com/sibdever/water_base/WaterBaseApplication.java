20784b65539f44f868d9bb63ec70bc175db872ba
Алексей Борзиков <a.borzikov@g.nsu.ru>
2021-02-21 16:13:53 +0700

    Initial commit

---
+++ src/main/java/com/sibdever/water_base/WaterBaseApplication.java
@@ -0,0 +1,13 @@
+package com.sibdever.water_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sibdever/water_base/WaterBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sibdever.water_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
